eecc53666b794df100c704f602f3893f35737de9
unknown <ctsjavauser115@iiht.tech>
2020-06-04 17:42:22 +0530

    Added routes in Angular UI

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ViewTaskComponent } from './components/view-task/view-task.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add-project', component: AddProjectComponent},
+  {path:'add-user', component: AddUserComponent},
+  {path:'add-task', component: AddTaskComponent},
+  {path:'view-task', component: ViewTaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
