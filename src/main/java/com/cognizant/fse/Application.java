fbc1386cbeb1799698f60109656bf07b08c9104f
unknown <ctsjavauser115@iiht.tech>
2020-06-07 20:19:48 +0530

    Added logging

--- src/main/java/com/cognizant/fse/Application.java
+++ src/main/java/com/cognizant/fse/Application.java
@@ -1,11 +1,16 @@
 package com.cognizant.fse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(Application.class);
+    public static void main(String[] args)
+    {
+        logger.info("Starting up projectmanager application");
         SpringApplication.run(Application.class, args);
     }
 }
