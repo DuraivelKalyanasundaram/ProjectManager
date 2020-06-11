5dfd1c6e332c6526d57af77cf5e85e5bab73cfac
unknown <ctsjavauser115@iiht.tech>
2020-06-11 17:58:17 +0530

    Initial version of add project functionality

--- ui/src/app/components/add-project/add-project.component.ts
+++ ui/src/app/components/add-project/add-project.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.scss']
 })
-export class AddProjectComponent implements OnInit {
+export class AddProjectComponent implements OnInit, AfterViewInit {
+
+  projectTitle = '';
+  startEndDateChecked = false;
+  @ViewChild('startDate') startDate : ElementRef;
+  @ViewChild('endDate') endDate : ElementRef;
+  startDateSelected;
+  endDateSelected;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    
+    const today = this.getToday();
+    this.startDate.nativeElement.setAttribute('value', today);
+    this.startDate.nativeElement.setAttribute('min', today);
+    this.endDate.nativeElement.setAttribute('value', this.getTomorrow());
+  }
+
+  startDateSelect() {
+    const newEndDate = this.getTomorrowFromDate(new Date(this.startDateSelected));
+    this.endDateSelected = newEndDate;
+    this.endDate.nativeElement.setAttribute('min', newEndDate);
+  }
+
+  private getToday() {
+    const dt = new Date();
+    let d = dt.getDate();
+    let mon = dt.getMonth() + 1;
+    let year = dt.getFullYear();
+    let month = '';
+    let day = '';
+    if (mon < 10) {
+      month = "0" + mon;
+    } else {
+      month = mon.toString();
+    }
+    if (d < 10) {
+      day = "0" + d;
+    } else {
+      day = d.toString();
+    }
+    return year + "-" + month + "-" + day;
+  }
+
+  private getTomorrow() {
+    const dt = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    let d = dt.getDate();
+    let mon = dt.getMonth() + 1;
+    let year = dt.getFullYear();
+    let month = '';
+    let day = '';
+    if (mon < 10) {
+      month = "0" + mon;
+    } else {
+      month = mon.toString();
+    }
+    if (d < 10) {
+      day = "0" + d;
+    } else {
+      day = d.toString();
+    }
+    return year + "-" + month + "-" + day;
+  }
+
+  private getTomorrowFromDate(dat: Date) {
+    const dt = new Date(dat.getTime() + 24 * 60 * 60 * 1000);
+    let d = dt.getDate();
+    let mon = dt.getMonth() + 1;
+    let year = dt.getFullYear();
+    let month = '';
+    let day = '';
+    if (mon < 10) {
+      month = "0" + mon;
+    } else {
+      month = mon.toString();
+    }
+    if (d < 10) {
+      day = "0" + d;
+    } else {
+      day = d.toString();
+    }
+    return year + "-" + month + "-" + day;
   }
 
 }
