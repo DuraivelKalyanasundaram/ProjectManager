484df46fcb72215467d91b7a56307233cbb54f9e
unknown <ctsjavauser115@iiht.tech>
2020-06-18 09:14:06 +0530

    New Model files for Task

---
+++ ui/src/app/model/TaskDTO.ts
@@ -0,0 +1,16 @@
+import { ParentTaskDTO } from './ParentTaskDTO';
+import { UserDTO } from './UserDTO';
+import { ProjectDTO } from './ProjectDTO';
+
+export class TaskDTO {
+    constructor(
+        public name: string,
+        public parentTask: ParentTaskDTO,
+        public startDate: string,
+        public endDate: string,
+        public priority: string,
+        public status: string,
+        public user: UserDTO,
+        public project: ProjectDTO
+    ) {}
+}
---
+++ ui/src/app/services/task.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Task } from '../model/Task';
+import { TaskDTO } from '../model/TaskDTO';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private BASE_URL = 'http://localhost:9090/tasks';
+  private httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+
+  constructor(private httpClient: HttpClient) { }
+
+  addTask(task: Task) {
+    return this.httpClient.post(this.BASE_URL, task, this.httpOptions);
+  }
+
+  getTasks() {
+    return this.httpClient.get<TaskDTO[]>(this.BASE_URL);
+  }
+
+  getTasksForProject(id: Number){
+    return this.httpClient.get<TaskDTO[]>(this.BASE_URL + '/project/' + id);
+  }
+
+}
